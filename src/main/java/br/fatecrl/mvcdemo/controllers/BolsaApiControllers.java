8f0790b0bc13b4dd1209bed0aeec9f3cde8a3c5a
Ozieni Costa <ozieni.costa@gmail.com>
2023-05-30 19:14:33 -0300

    alteração I

--- src/main/java/br/fatecrl/mvcdemo/controllers/BolsaApiControllers.java
+++ src/main/java/br/fatecrl/mvcdemo/controllers/BolsaApiControllers.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class BolsaApiControllers {
     private static final List<Bolsa> bolsas = new ArrayList<Bolsa>();
     public BolsaApiControllers() {
-        bolsas.add(new Bolsa("Ana", "M", 100));
+        bolsas.add(new Bolsa("Ana", "P", 100));
         bolsas.add(new Bolsa("Luna", "M", 110));
         bolsas.add(new Bolsa("Crystal", "P", 130));
         bolsas.add(new Bolsa("Sol", "M", 150));
--- src/main/java/br/fatecrl/mvcdemo/controllers/BolsaControllers.java
+++ src/main/java/br/fatecrl/mvcdemo/controllers/BolsaControllers.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class BolsaControllers {
     private static final List<Bolsa> bolsas = new ArrayList<Bolsa>();
     public BolsaControllers() {
-        bolsas.add(new Bolsa("Ana", "M", 100));
+        bolsas.add(new Bolsa("Ana", "P", 100));
         bolsas.add(new Bolsa("Luna", "M", 110));
         bolsas.add(new Bolsa("Crystal", "P", 130));
         bolsas.add(new Bolsa("Sol", "M", 150));
